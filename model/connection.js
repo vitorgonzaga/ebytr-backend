47db700132ce1d1f56e8a91640235200857ca4ad
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-02-11 16:34:07 -0300

    feat(conn): add module to connect database with MongoClient

---
+++ model/connection.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/';
+const DB_NAME = 'Cookmaster';
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+module.exports = () => MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => conn.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit();
+  });
