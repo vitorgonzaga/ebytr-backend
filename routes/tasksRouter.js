70c1b193c9d3fd9e609ab9fcfd541d317cd84e19
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-02-13 22:58:13 -0300

    feat(routes): add '/tasks/taskid' router for get method and '/tasks' for put method

--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth.middleware');
+// const checkifHasTaskId = require('../middlewares/checkTaskId.middleware');
 const {
   getAllTasksController,
   addTaskController,
+  updateTaskController,
+  getTaskIdController,
 } = require('../controllers/tasks.controller');
 
 router.get('/', auth, getAllTasksController);
+router.get('/taskid', auth, getTaskIdController);
 router.post('/', auth, addTaskController);
+router.put('/', auth, updateTaskController);
 
 module.exports = router;
