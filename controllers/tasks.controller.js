d4dbf7dd7127c68bba103125ef7219fd19c00b04
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-02-15 15:52:11 -0300

    refactor(tasks controller): take off some  variables
    take out declaration of taskUpdated variable to save return of updateTaskService, but it doesnt
    return json as default, so i prefer build the object into layer controller. Same situation with
    declaration of deletedTask variable. Both was removed. They are unnecessary.

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -52,8 +52,8 @@ const updateTaskController = async (req, res, next) => {
     // const { userId } = req;
     const { taskid } = req.params;
     const { task, status } = req.body;
-    const taskUpdated = await updateTaskService(taskid, task, status); // Não enviar o userId
-    return res.status(200).json(taskUpdated);
+    await updateTaskService(taskid, task, status); // Não enviar o userId
+    return res.status(200).json({ message: `${taskid} updated` });
   } catch (error) {
     console.log(error.message);
     next(error);
@@ -63,8 +63,8 @@ const updateTaskController = async (req, res, next) => {
 const removeTaskController = async (req, res, next) => {
   try {
     const { taskid } = req.params;
-    const deletedTask = await removeTaskService(taskid);
-    return res.status(200).json(deletedTask);
+    await removeTaskService(taskid);
+    return res.status(200).json({ message: `${taskid} removed` });
   } catch (error) {
     console.log(error.message);
     next(error);
